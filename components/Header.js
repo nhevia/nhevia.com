d0fc9a919beb20549d42ae89b448f8a992dcd20f
nhevia <nicolashevia@hotmail.com>
2021-12-24 00:27:42 +0100

    order header icons

--- components/Header.js
+++ components/Header.js
@@ -11,17 +11,17 @@ const Header = ({ theme, setTheme }) => {
 
   return (
     <div className="header-icons">
-      {theme === 'theme-light' ? (
-        <Sun onClick={handleTheme} />
-      ) : (
-        <Moon onClick={handleTheme} />
-      )}
       <a target="_blank" href="https://github.com/nhevia" rel="noreferrer">
         <Github />
       </a>
       <a target="_blank" href="https://twitter.com/n_hevia" rel="noreferrer">
         <Twitter />
       </a>
+      {theme === 'theme-light' ? (
+        <Sun onClick={handleTheme} />
+      ) : (
+        <Moon onClick={handleTheme} />
+      )}
     </div>
   );
 };
