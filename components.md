f7c3e3b2d67dfcbbf8550e8d53bedc14a051bffa
nhevia <nicolashevia@hotmail.com>
2021-12-24 00:49:27 +0100

    add documentation

---
+++ components.md
@@ -0,0 +1,6 @@
+[components/ContentList.js](#componentscontentlistjs)
+#### components/ContentList.js
+  Reusable and responsive component to render a list of items.
+- `data`: array of items from an XHR result.
+- `item`: a component that represents a single item of this list
+- `type`: declarative way to link style classNames
--- components/ContentList.js
+++ components/ContentList.js
@@ -1,3 +1,11 @@
+/*sdoc
+Reusable and responsive component to render a list of items.
+- `data`: array of items from an XHR result.
+- `item`: a component that represents a single item of this list
+- `type`: declarative way to link style classNames
+
+*/
+
 const ContentList = (props) => {
   return (
     <div className="content-list">
