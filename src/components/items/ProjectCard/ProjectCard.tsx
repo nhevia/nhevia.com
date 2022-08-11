59cf4bb3e4cefc7c137f75baf5cef246106a6d1a
nhevia <nicolashevia@hotmail.com>
2022-08-12 00:41:57 +0200

    refactor: remove unnecessary icons and unused data

--- src/components/items/ProjectCard/ProjectCard.tsx
+++ src/components/items/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Repo from 'public/icons/repo.svg';
-import Star from 'public/icons/star.svg';
-import Fork from 'public/icons/fork.svg';
 import { getLanguageColor } from 'utils/colors';
 import { Repository } from 'types/items';
 import s from './ProjectCard.module.css';
@@ -12,8 +10,6 @@ const ProjectCard = ({
   description,
   html_url,
   language,
-  stargazers_count,
-  forks,
 }: Repository) => {
   return (
     <div key={id} className={s.root} aria-label="repository summary">
@@ -52,15 +48,6 @@ const ProjectCard = ({
             }
           />
           <span data-testid="language">{language}</span>
-        </span>
-        <span className={s.section}>
-          <span>
-            <Star />
-          </span>
-          <span data-testid="stargazers">{stargazers_count}</span>
-        </span>
-        <span className={s.section} data-testid="forks">
-          <Fork /> {forks}
         </span>
       </div>
     </div>
