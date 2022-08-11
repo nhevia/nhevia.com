98d73c478202eace39c4c2b7e8942175fc682a65
Nico Hevia <50466554+nhevia@users.noreply.github.com>
2022-08-12 00:08:42 +0200

    refactor: normalize hierarchy for item card components (#8)

--- src/components/items/blogpost/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as BlogpostCard } from './BlogpostCard';
---
+++ src/components/items/index.ts
@@ -0,0 +1,3 @@
+export { default as BlogpostCard } from './BlogpostCard';
+export { default as ProjectFeaturedCard } from './ProjectFeaturedCard';
+export { default as ProjectCard } from './ProjectCard';
