19f77f3a9b9c9543331f450482138c731c807def
nhevia <nicolashevia@hotmail.com>
2022-08-12 19:36:02 +0200

    feat: get fresh github repository data every hour

--- pages/api/github-stats.ts
+++ pages/api/github-stats.ts
@@ -4,15 +4,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await fetch(
-    'https://api.stackexchange.com/2.2/users/6402990?site=stackoverflow'
-  );
-  const { quota_remaining } = await response.json();
-  console.log(response.headers);
+  const response = await fetch('https://api.github.com/users/nhevia/repos');
+  const data = await response.json();
 
   res.setHeader(
     'Cache-Control',
-    'max-age=60 s-maxage=60, stale-while-revalidate'
+    'max-age=3600 s-maxage=3600, stale-while-revalidate'
   );
-  res.status(200).json({ data: quota_remaining });
+  res.status(200).json(data);
 }
