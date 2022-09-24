923d9952bbf79605992aed923a3c7531715e801d
nhevia <nicolashevia@hotmail.com>
2022-09-24 03:02:07 +0200

    style: adds animated dash and social icons

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { showEmail } from 'utils/email';
 import s from './Footer.module.css';
+import Github from 'public/icons/github.svg';
+import Twitter from 'public/icons/twitter.svg';
+import Email from 'public/icons/email.svg';
 
 const Footer = () => {
   const [email, setEmail] = useState('wait a second');
@@ -13,16 +16,37 @@ const Footer = () => {
 
   return (
     <footer className={s.root}>
+      <svg className={s['dashline-container']}>
+        <line className={s.dashline} x1="0" y1="0" x2="3000" y2="0" />
+      </svg>
+
       <div className={s.sections}>
-        <a href={email} className={s.section}>
-          Contact me
-        </a>
-        <span className={s.divisor}></span>
-        <span className={s.section}>Nicolas Hevia @ 2022</span>
-        <span className={s.divisor}></span>
-        <a href="https://github.com/nhevia/nhevia.com" className={s.section}>
-          Site source code
-        </a>
+        <div className={s.socials}>
+          <a aria-label="email" href={email}>
+            <Email />
+          </a>
+          <a
+            aria-label="github"
+            target="_blank"
+            href="https://github.com/nhevia"
+            rel="noreferrer"
+          >
+            <Github />
+          </a>
+          <a
+            aria-label="twitter"
+            target="_blank"
+            href="https://twitter.com/n_hevia"
+            rel="noreferrer"
+          >
+            <Twitter />
+          </a>
+        </div>
+
+        <div className={s.info}>
+          <span className={s.divisor}>|</span>
+          <span>nico hevia © 2022</span>
+        </div>
       </div>
     </footer>
   );
